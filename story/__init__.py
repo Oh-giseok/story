ad70d644600cb5db69571a652f2100a7d856181b
CHOI-HYUNGSEUNG <sam1171102@gmail.com>
2026-09-22 16:11:27 +0900

    회원가입 기능 추가

--- story/__init__.py
+++ story/__init__.py
@@ -8,16 +8,21 @@ def create_app():
     app = Flask(__name__)
     migrate = Migrate()
 
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///story.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'dev-secret-key'
 
     db.init_app(app)
-
     migrate.init_app(app, db)
+
+    #블루프린트
+    from .views import main_views,auth_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     @app.route('/')
     def index():
         return "flaks team project"
 
     return app
+
--- story/models.py
+++ story/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False) #ID
     password_hash = db.Column(db.String(150), nullable=False) #PW
-    email = db.Column(db.String(200), unique=True, nuallable=False) #Email
-    name = db.COlumn(db.String(200),nullable=False) #이름
+    email = db.Column(db.String(200), unique=True, nullable=False) #Email
+    name = db.Column(db.String(200),nullable=False) #이름
     intro = db.Column(db.Text) #자기소개
     profile_img_url =db.Column(db.String(200)) #프로필 이미지
     birth = db.Column(db.DateTime, nullable=False) #생년월일
